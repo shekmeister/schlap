2a05ed03f4c72e8d47cb60cda3ba4fbce6dae76c
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2022-07-27 19:01:13 +0530

    Added Nested menu for Activity Status

---
+++ components/ActivityMenu.jsx
@@ -0,0 +1,18 @@
+import { Button, Menu } from '@mantine/core';
+
+export default function ActivityMenu() {
+    <>
+        <Menu
+            shadow='md'
+            position='right-start'
+            withArrow
+        >
+            <Menu.Target>
+                <Menu.Item>Activity status</Menu.Item>
+            </Menu.Target>
+            <Menu.Dropdown>
+
+            </Menu.Dropdown>
+        </Menu>
+    </>
+}
--- components/SidebarMenu.jsx
+++ components/SidebarMenu.jsx
@@ -1,4 +1,5 @@
 import { Menu, createStyles } from '@mantine/core';
+import ActivityMenu from './ActivityMenu'
 import 
 {   RiMore2Fill, 
     RiUserLine, 
@@ -16,17 +17,20 @@ const useStyles = createStyles((theme) => ({
     },
     item: {
         '&[data-hovered]': {
-            backgroundColor: theme.colors.tailwind.zinc['600']
+            backgroundColor: theme.colors.tailwind.zinc['600'],
         }
     },
     itemIcon: {
-        color: theme.colors.tailwind.zinc['400']
+        color: theme.colors.tailwind.zinc['400'],
     },
     itemLabel: {
-        color: 'white'
+        color: 'white',
     },
     divider: {
         borderTop: `1px solid ${theme.colors.tailwind.zinc['800']}`,
+    },
+    arrow: {
+        border: `1px solid ${theme.colors.tailwind.zinc['800']}`,
     }
 }));
 
@@ -35,6 +39,7 @@ export default function SidebarMenu() {
     return (
         <Menu width={200}
             shadow='md'
+            position='bottom'
             classNames={{
                 dropdown: classes.dropdown,
                 item: classes.item,
@@ -52,7 +57,35 @@ export default function SidebarMenu() {
                 <Menu.Divider></Menu.Divider>
                 <Menu.Label>User Settings</Menu.Label>
                 <Menu.Item icon={<RiUserLine size={20} />}>View Profile</Menu.Item>
-                <Menu.Item icon={<RiInformationLine size={20} />}>Activity Status</Menu.Item>
+                <Menu.Item >
+                    <Menu
+                        shadow='md'
+                        position='right-start'
+                        trigger='hover'
+                        width={150}
+                        offset={20}
+                        classNames={{
+                            dropdown: classes.dropdown,
+                            item: classes.item,
+                            itemLabel: classes.itemLabel,
+                            divider: classes.divider,
+                            itemIcon: classes.itemIcon,
+                            arrow: classes.arrow,
+                        }}
+                        withArrow
+                    >
+                        <Menu.Target>
+                            <Menu.Item component='a' sx={{ padding: '0px' }} icon={<RiInformationLine size={20} />}>Activity status</Menu.Item>
+                        </Menu.Target>
+                        <Menu.Dropdown>
+                            <Menu.Label>Set Status</Menu.Label>
+                            <Menu.Item>Online</Menu.Item>
+                            <Menu.Item>Offline</Menu.Item>
+                            <Menu.Item>Away</Menu.Item>
+                            <Menu.Item>Do not disturb</Menu.Item>
+                        </Menu.Dropdown>
+                    </Menu>
+                </Menu.Item>
                 <Menu.Divider></Menu.Divider>
                 <Menu.Label>Application</Menu.Label>
                 <Menu.Item icon={<RiSettings4Line size={20} />}>Settings</Menu.Item>
