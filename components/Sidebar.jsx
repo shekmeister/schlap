7ac1d0456bae56b9d954b4f01179b6ee9dbfa087
shekmeister <95183266+shekmeister@users.noreply.github.com>
2022-07-28 00:37:21 +0530

    Removed padding-top rules from Sidebar.jsx

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -5,8 +5,8 @@ import ChatPreview from './ChatPreview';
 export default function Sidebar() {
     return (
         <>
-            <div className='schlap-sidebar flex flex-col  h-screen w-full md:w-80 lg:w-96 bg-zinc-900'>
-                <div className='sidebar-header flex p-3 pt-0 border-zinc-600 border-b justify-between mb-3'>
+            <div className='schlap-sidebar flex flex-col h-screen w-full md:w-80 lg:w-96 bg-zinc-900'>
+                <div className='sidebar-header flex p-3 border-zinc-600 border-b justify-between mb-3'>
                     <h1 className='text-5xl md:text-3xl cursor-default'>Chats</h1>
                     <SidebarMenu />
                 </div>
