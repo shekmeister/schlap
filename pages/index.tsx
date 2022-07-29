df3b133c4759c7758859ff773d95274e5881e229
shekmeister <95183266+shekmeister@users.noreply.github.com>
2022-07-29 21:00:18 +0530

    Revert "Revert "Added login screen""
    This reverts commit 9a6ad1bdacd2efe6438de2addd748401fc5cb5a4.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { Paper } from '@mantine/core';
 import Sidebar from '../components/Sidebar';
 import ChatArea from '../components/ChatArea';
-import { useState } from 'react';
+import LogInForm from '../components/LogInForm';
 
 const Home: NextPage = () => {
   const [userState, setUser] = useState({
@@ -14,20 +16,36 @@ const Home: NextPage = () => {
     userFriends: null,
     userChats: null
   });
+  useEffect(() => {
+    function getUserInfo() {
+      const logStatus = JSON.parse(localStorage.getItem('isLoggedin'));
+      if (logStatus === true) {
+        setUser(prevState => ({
+          ...prevState,
+          isLoggedIn: true,
+        }));
+      };
+    }
+    getUserInfo()
+  }, []);
   return (
     <>
       <Head>
         <title>Schlap</title>
         <meta name="description" content="" />
       </Head>
-      <main className='schlap-body h-screen w-screen bg-black text-white'>
-        <div className='schlap-main-container flex'>
-          <Sidebar />
-          <ChatArea />
-        </div>
-      </main>
+      {(userState.isLoggedIn)?(
+        <main className='schlap-body h-screen w-screen bg-black text-white'>
+          <div className='schlap-main-container flex'>
+            <Sidebar />
+            <ChatArea />
+          </div>
+        </main>
+      ):(
+        <LogInForm />
+      )}
     </>
   )
 }
 
-export default Home
+export default Home;
