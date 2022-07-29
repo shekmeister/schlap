afb6fafa65d859d28bfe68ac592288702fd80b8a
shekmeister <95183266+shekmeister@users.noreply.github.com>
2022-07-29 12:23:36 +0530

    Fixed Sidebar Header heading alignment

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -6,7 +6,7 @@ export default function Sidebar() {
     return (
         <>
             <div className='schlap-sidebar flex flex-col h-screen w-full md:w-80 lg:w-96 bg-zinc-900 border-zinc-700 border-r'>
-                <div className='sidebar-header flex p-4 bg-zinc-800 border-zinc-600 h-16 border-b justify-between mb-3'>
+                <div className='sidebar-header flex items-center p-4 bg-zinc-800 border-zinc-600 h-16 border-b justify-between mb-3'>
                     <h1 className='text-5xl md:text-3xl cursor-default'>Chats</h1>
                     <SidebarMenu />
                 </div>
