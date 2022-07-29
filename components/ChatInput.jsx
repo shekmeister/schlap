74242b493e6d35cfd4930c7df6f15e0b4472ceac
shekmeister <95183266+shekmeister@users.noreply.github.com>
2022-07-29 10:40:56 +0530

    Added emoji Button and restyled Send button

--- components/ChatInput.jsx
+++ components/ChatInput.jsx
@@ -1,12 +1,16 @@
-import { Paper, createStyles, Button } from "@mantine/core";
-import { RiSendPlane2Line } from 'react-icons/ri';
+import { Paper, Tooltip } from "@mantine/core";
+import { FiSend } from 'react-icons/fi';
+import { TbMoodSmile } from 'react-icons/tb';
 
 export default function ChatInput() {
     return (
         <>
             <Paper className='bg-zinc-800 h-16 flex justify-around w-full shrink-0 flex-initial items-center' radius={0}>
-                <input className='schlap-chat-input p-3 text-white rounded bg-zinc-900 flex-initial w-96 focus:outline-0 focus:ring-1 focus:ring-zinc-600 transition-all'  type='text' placeholder='Type a message' />
-                <button className='text-zinc-200 bg-black text-bold p-3 pr-6 pl-6 rounded transition text-2xl'><RiSendPlane2Line /></button>
+                <input className='schlap-chat-input p-3 text-white rounded bg-zinc-900 flex-initial w-[45rem] focus:outline-0 focus:ring-1 focus:ring-zinc-600 transition-all mr-2 ml-2'  type='text' placeholder='Type a message' />
+                <Tooltip label='Emojis' color='black' withArrow>
+                    <button className='text-zinc-200 text-bold p-3 pr-3 pl-3 rounded transition text-2xl'><TbMoodSmile /></button>
+                </Tooltip>
+                <button className='text-zinc-200 text-bold p-3 pr-3 pl-3 rounded transition text-2xl'><FiSend /></button>
             </Paper>
         </>
     )
